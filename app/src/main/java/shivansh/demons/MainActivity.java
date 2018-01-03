42125551462a737183563cbc808b2797d22480ba
Shivansh Prakash <helloshivanshprakash@gmail.com>
2018-01-03 22:46:22 +0530

    Added a font to actionbar and updated collapsing toolbar imageview to display an image from gallery.

--- app/src/main/java/shivansh/demons/MainActivity.java
+++ app/src/main/java/shivansh/demons/MainActivity.java
@@ -1,26 +1,68 @@
 package shivansh.demons;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Spannable;
+import android.text.SpannableString;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int RESULT_LOAD_IMG = 1;
     Database dbHelper;
+    String imgDecodableString;
     private MyAdapter mAdapter;
     private RecyclerView Task;
+    private String PREFS_NAME = "image";
+    private Context mContext;
+    private String[] galleryPermissions = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SpannableString s = new SpannableString("Demons");
+        s.setSpan(new shivansh.demons.TypefaceSpan(this, "Courgette-Regular.ttf"), 0, s.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+// Update the action bar title with the TypefaceSpan instance
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(s);
+        try {
+            mContext = this;
+            ImageView imageView = (ImageView) findViewById(R.id.bgheader);
+            String path = getPreference(mContext, "imagePath");
+
+            if (!(path == null || path.length() == 0 || path.equalsIgnoreCase(""))) {
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 4;
+                Bitmap bitmap = BitmapFactory.decodeFile(path, options);
+                imageView.setImageBitmap(bitmap);
+                //imageView.setImageBitmap(getScaledBitmap(path, 800, 800));
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         dbHelper = new Database(this);
         loadTaskList();
     }
@@ -64,6 +106,13 @@ public class MainActivity extends AppCompatActivity {
                 dialog.show();
                 loadTaskList();
                 return true;
+            case R.id.change_pic:
+                if (!checkIfAlreadyhavePermission()) {
+                    ActivityCompat.requestPermissions(this, galleryPermissions, 1);
+                } else {
+                    Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+                }
         }
         return super.onOptionsItemSelected(item);
     }
@@ -78,4 +127,69 @@ public class MainActivity extends AppCompatActivity {
         loadTaskList();
     }
     */
+    private boolean checkIfAlreadyhavePermission() {
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+                } else {
+                    Toast.makeText(this, "Please give your permission.", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+                setPreference(mContext, imgDecodableString, "imagePath");
+                ImageView imgView = (ImageView) findViewById(R.id.bgheader);
+                // Set the Image in ImageView after decoding the String
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 4;
+                Bitmap bitmap = BitmapFactory.decodeFile(imgDecodableString, options);
+                imgView.setImageBitmap(bitmap);
+                //BitmapFactory.decodeFile(imgDecodableString)
+            } else {
+                Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    void setPreference(Context c, String value, String key) {
+        SharedPreferences settings = c.getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    String getPreference(Context c, String key) {
+        SharedPreferences settings = c.getSharedPreferences(PREFS_NAME, 0);
+        return settings.getString(key, "");
+    }
 }
